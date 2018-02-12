6fc1d60dfcfdd5489ffd2dc2e0d323da99d624b8
ziotonio <35451667+ziotonio@users.noreply.github.com>
2018-02-12 18:06:56 +0100

    Add files via upload

--- src/cn/itcast/invoice/invoice/bill/business/ebo/BillEbo.java
+++ src/cn/itcast/invoice/invoice/bill/business/ebo/BillEbo.java
@@ -34,7 +34,8 @@ import cn.itcast.invoice.util.jxl.ExcelUtil;
  */
 public class BillEbo implements BillEbi{
 	public static int twenty = 20;
-	public static int fourteen = 14, twelve = 12, ten = 10;
+	public static int fourteen = 14, twelve = 12, ten = 10, fourfivesix = 456, threesixO = 360, one = 1, two = 2, three = 3, four = 4, five = 5, 
+			twentyfive = 25, fifteen = 15, thirtyseven = 37, zero = 0, six = 6, twentythree = 23, eight = 8, twentyfour = 24, eighteen = 18;
 	public static double zerozero2D = 0.02D;
 	private static final GoodsModel[] objs = null;
 
@@ -77,12 +78,12 @@ public class BillEbo implements BillEbi{
 		//ÃƒÂ¦Ã¢â‚¬Â¢Ã‚Â°ÃƒÂ¦Ã¯Â¿Â½Ã‚Â®ÃƒÂ¨Ã‚Â½Ã‚Â¬ÃƒÂ¥Ã…â€™Ã¢â‚¬â€œÃƒÂ¤Ã‚Â¸Ã‚ÂºjfreechartÃƒÂ¥Ã‚Â¯Ã‚Â¹ÃƒÂ¨Ã‚Â±Ã‚Â¡
 		JFreeChart localJFreeChart = ChartFactory.createPieChart("ÃƒÂ©Ã¢â‚¬Â¡Ã¢â‚¬Â¡ÃƒÂ¨Ã‚Â´Ã‚Â­ÃƒÂ§Ã‚Â»Ã…Â¸ÃƒÂ¨Ã‚Â®Ã‚Â¡", localDefaultPieDataset, true, true, false);
 		PiePlot localPiePlot = (PiePlot) localJFreeChart.getPlot();
-		localPiePlot.setLabelFont(new Font("SansSerif", 0, twelve));
+		localPiePlot.setLabelFont(new Font("SansSerif", zero, twelve));
 		localPiePlot.setNoDataMessage("No data available");
 		localPiePlot.setLabelGap(zerozero2D);
 		//jfreechartÃƒÂ¥Ã‚Â¯Ã‚Â¹ÃƒÂ¨Ã‚Â±Ã‚Â¡ÃƒÂ¨Ã‚Â£Ã¢â‚¬Â¦ÃƒÂ¥Ã¢â‚¬Â¦Ã‚Â¥osÃƒÂ¥Ã‚Â¯Ã‚Â¹ÃƒÂ¨Ã‚Â±Ã‚Â¡
 		try {
-			ImageIO.write(localJFreeChart.createBufferedImage(456, 360), "png", os);
+			ImageIO.write(localJFreeChart.createBufferedImage(fourfivesix, threesixO), "png", os);
 		} catch (IOException e) {
 			 System.out.println("Something was wrong");
 		}
@@ -94,52 +95,52 @@ public class BillEbo implements BillEbi{
 		WritableWorkbook b = ExcelUtil.cWorkbook(os); 
 		
 		//WritableSheet s = workbook.createSheet("ÃƒÂ¦Ã¢â€šÂ¬Ã‚Â»ÃƒÂ¦Ã¢â‚¬Â¹Ã‚Â¬", 0); 
-		WritableSheet s = ExcelUtil.cSheet(b, 1, "ÃƒÂ¦Ã¢â€šÂ¬Ã‚Â»ÃƒÂ¦Ã¢â‚¬Â¹Ã‚Â¬");
+		WritableSheet s = ExcelUtil.cSheet(b, one, "ÃƒÂ¦Ã¢â€šÂ¬Ã‚Â»ÃƒÂ¦Ã¢â‚¬Â¹Ã‚Â¬");
 		
 		//ÃƒÂ¨Ã‚Â®Ã‚Â¾ÃƒÂ§Ã‚Â½Ã‚Â®ÃƒÂ¨Ã‚Â¡Ã…â€™ÃƒÂ©Ã‚Â«Ã‹Å“
-		ExcelUtil.sRowSize(s, 1, 15);
-		ExcelUtil.sRowSize(s, 2, 37);
-		ExcelUtil.sRowSize(s, 3, 6);
-		ExcelUtil.sRowSize(s, 4, 23);
+		ExcelUtil.sRowSize(s, one, fifteen);
+		ExcelUtil.sRowSize(s, two, thirtyseven);
+		ExcelUtil.sRowSize(s, three, six);
+		ExcelUtil.sRowSize(s, four, twentythree);
 		
 		//ÃƒÂ¨Ã‚Â®Ã‚Â¾ÃƒÂ§Ã‚Â½Ã‚Â®ÃƒÂ¥Ã‹â€ Ã¢â‚¬â€�ÃƒÂ¥Ã‚Â®Ã‚Â½
-		ExcelUtil.sColumnSize(s, 1, 8);
-		ExcelUtil.sColumnSize(s, 2, 8);
-		ExcelUtil.sColumnSize(s, 3, 25);
-		ExcelUtil.sColumnSize(s, 4, 25);
-		ExcelUtil.sColumnSize(s, 5, 25);
+		ExcelUtil.sColumnSize(s, one, eight);
+		ExcelUtil.sColumnSize(s, two, eight);
+		ExcelUtil.sColumnSize(s, three, twentyfive);
+		ExcelUtil.sColumnSize(s, four, twentyfive);
+		ExcelUtil.sColumnSize(s, five, twentyfive);
 		
 		//ÃƒÂ¥Ã¯Â¿Â½Ã‹â€ ÃƒÂ¥Ã‚Â¹Ã‚Â¶
-		ExcelUtil.sMerge(s, 2 , 2 , 2 , 4);
-		ExcelUtil.sMerge(s, 3 , 2 , 3 , 5);
+		ExcelUtil.sMerge(s, two , two , two , four);
+		ExcelUtil.sMerge(s, three , two , three , five);
 		
 		//ÃƒÂ¨Ã‚Â¿Ã¢â‚¬ÂºÃƒÂ¨Ã‚Â´Ã‚Â§ÃƒÂ§Ã‚Â»Ã…Â¸ÃƒÂ¨Ã‚Â®Ã‚Â¡ÃƒÂ¦Ã…Â Ã‚Â¥ÃƒÂ¨Ã‚Â¡Ã‚Â¨		
-		Label lab22 = ExcelUtil.cLabel(2, 2, "ÃƒÂ¨Ã‚Â¿Ã¢â‚¬ÂºÃƒÂ¨Ã‚Â´Ã‚Â§ÃƒÂ§Ã‚Â»Ã…Â¸ÃƒÂ¨Ã‚Â®Ã‚Â¡ÃƒÂ¦Ã…Â Ã‚Â¥ÃƒÂ¨Ã‚Â¡Ã‚Â¨");
-		ExcelUtil.sLabelStyle(lab22, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", 24, Colour.BLACK, Colour.LIGHT_BLUE, 1, "2020");
+		Label lab22 = ExcelUtil.cLabel(two, two, "ÃƒÂ¨Ã‚Â¿Ã¢â‚¬ÂºÃƒÂ¨Ã‚Â´Ã‚Â§ÃƒÂ§Ã‚Â»Ã…Â¸ÃƒÂ¨Ã‚Â®Ã‚Â¡ÃƒÂ¦Ã…Â Ã‚Â¥ÃƒÂ¨Ã‚Â¡Ã‚Â¨");
+		ExcelUtil.sLabelStyle(lab22, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", twentyfour, Colour.BLACK, Colour.LIGHT_BLUE, one, "2020");
 		ExcelUtil.aLabelToSheet(lab22, s);
 		//ÃƒÂ¤Ã‚Â¸Ã¯Â¿Â½ÃƒÂ©Ã¢â€žÂ¢Ã¯Â¿Â½
-		Label lab25 = ExcelUtil.cLabel(2, 5, "ÃƒÂ¤Ã‚Â¸Ã¯Â¿Â½ÃƒÂ©Ã¢â€žÂ¢Ã¯Â¿Â½");
-		ExcelUtil.sLabelStyle(lab25, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", 12, Colour.BLACK, Colour.LIGHT_BLUE, 1, "2002");
+		Label lab25 = ExcelUtil.cLabel(two, five, "ÃƒÂ¤Ã‚Â¸Ã¯Â¿Â½ÃƒÂ©Ã¢â€žÂ¢Ã¯Â¿Â½");
+		ExcelUtil.sLabelStyle(lab25, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", twelve, Colour.BLACK, Colour.LIGHT_BLUE, one, "2002");
 		ExcelUtil.aLabelToSheet(lab25, s);
 		
-		Label lab32 = ExcelUtil.cLabel(3, 2, "");
-		ExcelUtil.sLabelStyle(lab32, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", 1, Colour.BLACK, Colour.GRAY_25, 1, "2022");
+		Label lab32 = ExcelUtil.cLabel(three, two, "");
+		ExcelUtil.sLabelStyle(lab32, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", one, Colour.BLACK, Colour.GRAY_25, one, "2022");
 		ExcelUtil.aLabelToSheet(lab32, s);
 		
-		Label lab42 = ExcelUtil.cLabel(4, 2, "ÃƒÂ§Ã‚Â¼Ã¢â‚¬â€œÃƒÂ¥Ã¯Â¿Â½Ã‚Â·");
-		ExcelUtil.sLabelStyle(lab42, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", 18, Colour.BLACK, null, 1, "2220");
+		Label lab42 = ExcelUtil.cLabel(four, two, "ÃƒÂ§Ã‚Â¼Ã¢â‚¬â€œÃƒÂ¥Ã¯Â¿Â½Ã‚Â·");
+		ExcelUtil.sLabelStyle(lab42, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", eighteen, Colour.BLACK, null, one, "2220");
 		ExcelUtil.aLabelToSheet(lab42, s);
 		
-		Label lab43 = ExcelUtil.cLabel(4, 3, "ÃƒÂ¥Ã…Â½Ã¢â‚¬Å¡ÃƒÂ¥Ã¢â‚¬Â¢Ã¢â‚¬Â ");
-		ExcelUtil.sLabelStyle(lab43, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", 18, Colour.BLACK, null, 1, "2220");
+		Label lab43 = ExcelUtil.cLabel(four, three, "ÃƒÂ¥Ã…Â½Ã¢â‚¬Å¡ÃƒÂ¥Ã¢â‚¬Â¢Ã¢â‚¬Â ");
+		ExcelUtil.sLabelStyle(lab43, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", eighteen, Colour.BLACK, null, one, "2220");
 		ExcelUtil.aLabelToSheet(lab43, s);
 		
-		Label lab44 = ExcelUtil.cLabel(4, 4, "ÃƒÂ¥Ã¢â‚¬Â¢Ã¢â‚¬Â ÃƒÂ¥Ã¢â‚¬Å“Ã¯Â¿Â½ÃƒÂ¥Ã¯Â¿Â½Ã¯Â¿Â½");
-		ExcelUtil.sLabelStyle(lab44, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", 18, Colour.BLACK, null, 1, "2220");
+		Label lab44 = ExcelUtil.cLabel(four, four, "ÃƒÂ¥Ã¢â‚¬Â¢Ã¢â‚¬Â ÃƒÂ¥Ã¢â‚¬Å“Ã¯Â¿Â½ÃƒÂ¥Ã¯Â¿Â½Ã¯Â¿Â½");
+		ExcelUtil.sLabelStyle(lab44, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", eighteen, Colour.BLACK, null, one, "2220");
 		ExcelUtil.aLabelToSheet(lab44, s);
 		
-		Label lab45 = ExcelUtil.cLabel(4, 5, "ÃƒÂ¦Ã¢â‚¬Â¢Ã‚Â°ÃƒÂ©Ã¢â‚¬Â¡Ã¯Â¿Â½");
-		ExcelUtil.sLabelStyle(lab45, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", 18, Colour.BLACK, null, 1, "2222");
+		Label lab45 = ExcelUtil.cLabel(four, five, "ÃƒÂ¦Ã¢â‚¬Â¢Ã‚Â°ÃƒÂ©Ã¢â‚¬Â¡Ã¯Â¿Â½");
+		ExcelUtil.sLabelStyle(lab45, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", eighteen, Colour.BLACK, null, one, "2222");
 		ExcelUtil.aLabelToSheet(lab45, s);
 					
 		int row = 4;
@@ -150,20 +151,20 @@ public class BillEbo implements BillEbi{
 			GoodsModel gm = (GoodsModel)objs[0];
 			Long num = (Long)objs[1];
 			
-			Label lab_data_1 = ExcelUtil.cLabel(row+i, 2, i+"");
-			ExcelUtil.sLabelStyle(lab_data_1, "ÃƒÂ¥Ã‚Â®Ã¢â‚¬Â¹ÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", 14, Colour.BLACK, null, 1, "0120");
+			Label lab_data_1 = ExcelUtil.cLabel(row+i, two, i+"");
+			ExcelUtil.sLabelStyle(lab_data_1, "ÃƒÂ¥Ã‚Â®Ã¢â‚¬Â¹ÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", fourteen, Colour.BLACK, null, one, "0120");
 			ExcelUtil.aLabelToSheet(lab_data_1, s);
 			
-			Label lab_data_2 = ExcelUtil.cLabel(row+i, 3, gm.getGtm().getSm().getName());
-			ExcelUtil.sLabelStyle(lab_data_2, "ÃƒÂ¥Ã‚Â®Ã¢â‚¬Â¹ÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", 14, Colour.BLACK, null, 1, "0110");
+			Label lab_data_2 = ExcelUtil.cLabel(row+i, three, gm.getGtm().getSm().getName());
+			ExcelUtil.sLabelStyle(lab_data_2, "ÃƒÂ¥Ã‚Â®Ã¢â‚¬Â¹ÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", fourteen, Colour.BLACK, null, one, "0110");
 			ExcelUtil.aLabelToSheet(lab_data_2, s);
 			
-			Label lab_data_3 = ExcelUtil.cLabel(row+i, 4, gm.getName());
-			ExcelUtil.sLabelStyle(lab_data_3, "ÃƒÂ¥Ã‚Â®Ã¢â‚¬Â¹ÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", 14, Colour.BLACK, null, 1, "0110");
+			Label lab_data_3 = ExcelUtil.cLabel(row+i, four, gm.getName());
+			ExcelUtil.sLabelStyle(lab_data_3, "ÃƒÂ¥Ã‚Â®Ã¢â‚¬Â¹ÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", fourteen, Colour.BLACK, null, one, "0110");
 			ExcelUtil.aLabelToSheet(lab_data_3, s);
 			
-			Label lab_data_4 = ExcelUtil.cLabel(row+i, 5, num.toString());
-			ExcelUtil.sLabelStyle(lab_data_4, "ÃƒÂ¥Ã‚Â®Ã¢â‚¬Â¹ÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", 14, Colour.BLACK, null, 1, "0112");
+			Label lab_data_4 = ExcelUtil.cLabel(row+i, four, num.toString());
+			ExcelUtil.sLabelStyle(lab_data_4, "ÃƒÂ¥Ã‚Â®Ã¢â‚¬Â¹ÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", fourteen, Colour.BLACK, null, one, "0112");
 			ExcelUtil.aLabelToSheet(lab_data_4, s);
 			
 			sum += num;
@@ -171,16 +172,16 @@ public class BillEbo implements BillEbi{
 		}
 		
 		//ÃƒÂ¨Ã‚Â®Ã‚Â¾ÃƒÂ§Ã‚Â½Ã‚Â®ÃƒÂ¨Ã‚Â¡Ã…â€™ÃƒÂ©Ã‚Â«Ã‹Å“
-		ExcelUtil.sRowSize(s, row+i, 24);
+		ExcelUtil.sRowSize(s, row+i, twentyfour);
 		//ÃƒÂ¥Ã¯Â¿Â½Ã‹â€ ÃƒÂ¥Ã‚Â¹Ã‚Â¶
-		ExcelUtil.sMerge(s,row+i  , 2 , row+i , 4);
+		ExcelUtil.sMerge(s,row+i  , two , row+i , four);
 		
-		Label lab_toatl_tital = ExcelUtil.cLabel(row+i, 2, "ÃƒÂ¦Ã¢â€šÂ¬Ã‚Â»ÃƒÂ¨Ã‚Â®Ã‚Â¡");
-		ExcelUtil.sLabelStyle(lab_toatl_tital, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", 18, Colour.BLACK, null, 1, "2220");
+		Label lab_toatl_tital = ExcelUtil.cLabel(row+i, two, "ÃƒÂ¦Ã¢â€šÂ¬Ã‚Â»ÃƒÂ¨Ã‚Â®Ã‚Â¡");
+		ExcelUtil.sLabelStyle(lab_toatl_tital, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", eighteen, Colour.BLACK, null, one, "2220");
 		ExcelUtil.aLabelToSheet(lab_toatl_tital, s);
 		
-		Label lab_toatl_data = ExcelUtil.cLabel(row+i, 5, sum.toString());
-		ExcelUtil.sLabelStyle(lab_toatl_data, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", 18, Colour.BLACK, null, 1, "2222");
+		Label lab_toatl_data = ExcelUtil.cLabel(row+i, five, sum.toString());
+		ExcelUtil.sLabelStyle(lab_toatl_data, "ÃƒÂ©Ã‚Â»Ã¢â‚¬ËœÃƒÂ¤Ã‚Â½Ã¢â‚¬Å“", eighteen, Colour.BLACK, null, one, "2222");
 		ExcelUtil.aLabelToSheet(lab_toatl_data, s);
 					
 		b.write(); 
